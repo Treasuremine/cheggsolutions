e5e38f15eb80ec5a10664327fd485221dbc51b49
Mallika <132191889+Treasuremine@users.noreply.github.com>
2023-06-09 10:26:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,34 +9,3 @@ server.listen( 8080, () =>{
   console.log('Server running at http://localhost:8080/');
 });
 console.log("Hello,thesathicoaching!");
-
-await asyncFunction();
-
-function normalFunction() {
-  doSomething();
-}
-
-async function f1() {
-  await asyncFunction();
-}
-
-const f2 = async () => {
-  await asyncFunction();
-};
-
-const f3 = async () => await asyncFunction();
-
-async function f4() {
-  for await (const num of asyncIterable) {
-    console.log(num);
-  }
-}
-
-// empty functions are valid
-async function emptyFunction() {}
-const emptyArrowFunction = async () => {};
-
-// generators are also valid
-async function* gen() {
-  console.log(42);
-}
